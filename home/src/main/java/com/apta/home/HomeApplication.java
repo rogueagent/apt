636d313b0727ce91b2681f0d72c6b8669c0d23b4
rogueagent <piyushbhawan@gmail.com>
2018-01-21 13:19:56 +1100

    first commit again

---
+++ home/src/main/java/com/apta/home/HomeApplication.java
@@ -0,0 +1,12 @@
+package com.apta.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeApplication.class, args);
+	}
+}
